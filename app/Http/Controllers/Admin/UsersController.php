60edc34e3be410420dc8a8a7126c8daaba03f13d
corean <corean@corean.biz>
2016-01-04 18:14:17 +0900

    in office

--- app/Http/Controllers/Admin/UsersController.php
+++ app/Http/Controllers/Admin/UsersController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
-
+use App\Role;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use App\User;
@@ -14,5 +14,28 @@ class UsersController extends Controller
     {
         $users = User::all();
         return view('backend.users.index', compact('users'));
+    }
+
+    public function edit($id)
+    {
+        $user = User::whereId($id)->firstOrFail();
+        $roles = Role::all();
+        $selectedRoles = $user->roles->lists('id')->toArray();
+        return view('backend.users.edit', compact('user', 'roles', 'selectedRoles'));
+    }
+
+    public function update($id, UserEditFormRequest $request)
+    {
+        $user = User::whereId($id)->firstOrFail();
+        $user->name = $request->get('name');
+        $user->email = $request->get('email');
+        $password = $request->get('password');
+        if($password != "") {
+            $user->password = Hash::make($password);
+        }
+        $user->save();
+        $user->saveRoles($request->get('role'));
+
+        return redirect(action('Admin\UsersController@edit', $user->id))->with('status', 'The user has been updated!');
     }
 }
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -54,6 +54,11 @@ Route::group(['middleware' => ['web']], function () {
         return redirect('/');
     });
     Route::group(array('prefix' => 'admin', 'namespace' => 'Admin', 'middleware' => 'manager'), function () {
-        Route::get('users', 'UsersController@index');
+        Route::get('users', [ 'as' => 'admin.user.index', 'uses' => 'UsersController@index']);
+        Route::get('roles', 'RolesController@index');
+        Route::get('roles/create', 'RolesController@create');
+        Route::post('roles/create', 'RolesController@store');
+        Route::get('users/{id?}/edit', 'UsersController@edit');
+        Route::post('users/{id?}/edit','UsersController@update');
     });
 });
